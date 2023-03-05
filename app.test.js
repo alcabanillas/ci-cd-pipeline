9aa642d7807e7e5217262e299a75dfb3ab46ac9c
alcabanillas <acabanillas@gmail.com>
2023-03-05 16:51:37 +0100

    try again

--- app.test.js
+++ app.test.js
@@ -3,7 +3,7 @@ const app = require('./app')
 
 
 describe("GET /todos", () => {
-  it("Should find 'Hello World!' in the DOM 2", async () => {
+  it("Should find 'Hello World!' in the DOM", async () => {
     const response = await request(app).get("/")
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Hello World!");
